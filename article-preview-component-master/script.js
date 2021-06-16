45f123095d9a714e52d485b4c8ee9f8e80a4d909
Andrei Stefan <82610764+RO-Stefan-Andrei@users.noreply.github.com>
2021-06-16 11:05:37 +0300

    Delete article-preview-component-master directory

--- article-preview-component-master/script.js
+++
@@ -1,7 +0,0 @@
-const myBtn = document.querySelector(".myBtn");
-const shareBtn = document.querySelector(".partajare");
-
-myBtn.addEventListener("click" , function(){
-    shareBtn.classList.toggle("partajare");
-    shareBtn.classList.add("active");
-})
